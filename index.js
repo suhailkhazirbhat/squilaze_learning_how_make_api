385db5ad4af4517319ba04e9084b180c0915571a
suhailkhazirbhat <suhailbhatb7@gmail.com>
2023-09-25 17:35:55 +0530

    add

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000,()=>{
+    console.log("App will run on:3000")
+})
---
+++ model/index.js
@@ -0,0 +1,12 @@
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql'/* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+  });
+  try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+//  
